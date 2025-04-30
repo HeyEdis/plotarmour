38eec6b1d224647fa0647c4bb011ace6aaca55e4
Edis Hurtic <edis.hurtic@student.hogent.be>
2025-04-30 18:55:14 +0200

    adding syntax checking

---
+++ eslint.config.js
@@ -0,0 +1,11 @@
+// eslint.config.js
+// de regels aan wat de code moet voldoen
+export default [
+    {
+      files: ["**/*.js"],
+      languageOptions: {
+        ecmaVersion: "latest",
+      },
+      rules: {},
+    },
+  ];
